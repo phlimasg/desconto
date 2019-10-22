4d4c1e99a7daf9462dc70b1ee3183b92a7700a09
phlimasg <raphaelpc_@hotmail.com>
2019-10-22 09:18:38 -0300

    Update index.blade.php

--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -8,7 +8,8 @@
     {{ $candidato->links() }}
     <div class="container-fluid">
         @foreach ($candidato as $c)
-            <div class="well well-lg" style="border: 1px;">
+        @if(strstr($c->id_cand,'2020'))
+            <div class="well well-lg alert-success" style="border: 1px;">
                 <div class="row">
                     <div class="col-sm-1">
                         <label for="">Id</label><br>
@@ -66,7 +67,68 @@
                     </div>
 
                 </div>
-            </div>
+            </div> 
+            @else
+            <div class="well well-lg" style="border: 1px;">
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <label for="">Id</label><br>
+                            {{ strtoupper( $c->id_cand)}}
+                        </div>
+                        <div class="col-sm-4">
+                            <label for="">Candidato</label><br>
+                            {{ strtoupper( $c->nome_cand)}}
+                        </div>
+                        <div class="col-sm-1">
+                            <label for="">Solicitação</label><br>                        
+                            {{date('d/m/Y', strtotime($c->created_at))}}
+                        </div>
+                        <div class="col-sm-1">
+                            <label for="">Percentual</label><br>
+                            @if(stripos($c->desc_cand,'%'))
+                                {{$c->desc_cand}}
+                            @elseif($c->desc_cand != null)
+                                {{$c->desc_cand}}%
+                            @else
+                            @endif
+                        </div>
+                        <div class="col-sm-1">
+                            <label for="">Sugerido</label><br>
+                            @if(!empty($c->desc_sug))
+                                @if(stripos($c->desc_sug,'%'))
+                                    {{$c->desc_sug}}
+                                @else
+                                    {{$c->desc_sug}}%
+                                @endif
+    
+                            @endif
+                        </div>
+                        <div class="col-sm-1">
+                            <label for="">Aprovado</label><br>
+                            @if(!empty($c->desc_aut))
+                                @if(stripos($c->desc_aut,'%'))
+                                    {{$c->desc_aut}}
+                                @else
+                                    {{$c->desc_aut}}%
+                                @endif
+    
+                            @endif
+                        </div>
+                        <div class="col-sm-1">
+                            <label for="">Status</label><br>
+                            {{$c->status_desc}}
+                        </div>
+                        <div class="col-sm-1">
+                        </div>
+                    </div>
+                    <div class="row text-right">
+                        <div class="col-sm-12">
+                            <a href="{{route('mCandidato',['id' => Request::segment(2),'mat' => $c->id_cand])}}" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span> Analisar</a>
+                        </div>
+    
+                    </div>
+                </div>       
+        @endif
             <hr>
         @endforeach
     </div>
